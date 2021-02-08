ecdde90b7c975c8bca7daea814c9cc38ffc6fa99
keerthi-battula <net2bks@gmail.com>
2021-02-08 09:45:16 +0530

    third commit

--- GitFun.java
+++ GitFun.java
@@ -6,5 +6,7 @@ public static void main(String args[])
 	int c=a+b;
 	System.out.println("the sum of a and b is:");
 	System.out.println(c);
+	System.out.println("thank you!");
+	System.out.println("please visit again!");
 }
 }
